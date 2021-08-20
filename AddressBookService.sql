4e3c39953eee6bb0f2ac838eddbd7cec23c3ed99
Akshithaaashi <akshitha.aashi@gmail.com>
2021-08-20 13:23:06 +0530

    Added : to create a address book table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -4,3 +4,17 @@
 create database address_book_service;
 show databases; 
 use address_book_service;
+
+#uc2
+#Ability to create a Address Book Table with first andlast names, address, city,state, zip, phone numberand email as its attributes.
+create table address_book_service (
+first_name varchar(31) not null,
+last_name varchar(31) not null,
+address varchar(150) not null,
+city varchar(31) not null,
+state varchar(31) not null,
+zip int not null,
+phone_number int not null,
+email varchar(50) not null
+);
+select * from address_book_service;
